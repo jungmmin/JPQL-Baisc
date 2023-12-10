3ebfbb12291e068e0600d7d753fd90bb54ba3410
정민 <ilb0919497@gmail.com>
2023-12-10 14:19:13 +0900

    jpql SELCT 예제

--- src/test/java/jpql/jpqlList/JpqlSelect.java
+++ src/test/java/jpql/jpqlList/JpqlSelect.java
@@ -1,6 +1,7 @@
 package jpql.jpqlList;
 
 import jpql.Member;
+import jpql.MemberDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Transactional
-class JpqlTest {
+class JpqlSelect {
 
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
@@ -38,6 +39,34 @@ class JpqlTest {
                 System.out.println("member1 = " + member1.getUsername());
             }
             System.out.println("=============================");
+
+            // 롤백으로 데이터 초기화
+            tx.rollback();
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+    }
+
+    @Test
+    @DisplayName("여러개의 값 검색")
+    void selectList() {
+        tx.begin();
+        try {
+            // 멤버 생성
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            // 여러개의 컬럼 검색
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
+
+            MemberDTO findItem = resultList.get(0);
+            System.out.println("memberDTO name = " + findItem.getUsername());
+            System.out.println("memberDTO age = " + findItem.getAge());
 
             // 롤백으로 데이터 초기화
             tx.rollback();
