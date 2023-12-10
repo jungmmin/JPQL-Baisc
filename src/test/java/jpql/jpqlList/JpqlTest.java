36379c558b1e28dd53fbbf2a67176ef269a05387
정민 <ilb0919497@gmail.com>
2023-12-10 15:44:06 +0900

    조인 추가

--- src/test/java/jpql/jpqlList/JpqlTest.java
+++ src/test/java/jpql/jpqlList/JpqlTest.java
@@ -1,6 +1,7 @@
 package jpql.jpqlList;
 
 import jpql.Member;
+import jpql.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Transactional
-public class JpqlPaging {
+public class JpqlTest {
 
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
@@ -37,6 +38,45 @@ public class JpqlPaging {
             List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
                     .setFirstResult(0)
                     .setMaxResults(10)
+                    .getResultList();
+
+            System.out.println("resultList.size() = " + resultList.size());
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+
+
+            // 롤백으로 데이터 초기화
+            tx.rollback();
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+    }
+
+    @Test
+    @DisplayName("조인 예시")
+    void joinTest() {
+        tx.begin();
+        try {
+            // 멤버 생성
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            // 파라미터 바인딩 사용
+            List<Member> resultList = em.createQuery("select m from Member m inner join m.team t", Member.class)
                     .getResultList();
 
             System.out.println("resultList.size() = " + resultList.size());
